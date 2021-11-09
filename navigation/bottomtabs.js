f9bc9ce45f909ed03535062e9d4c122d4875fd9c
yamin83i <mashabulyamin99@gmail.com>
2021-11-09 09:16:38 +0700

    Finishing sign up screeen

---
+++ navigation/bottomtabs.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import { createBottomTabNavigator, BottomTabBar } from "@react-navigation/bottom-tabs"
+import Svg,{Path} from 'react-native-svg'
+
+import {Home, Scan} from "../screens"
+import { COLORS,icons } from '../constants'
+
